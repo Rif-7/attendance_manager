decf0a9f4be3e902753e4ee2eb4af27eec734a65
Rif-7 <muhammadrifan974530@gmail.com>
2024-03-24 16:08:46 +0530

    edit Class schema with new fields

--- models/Class.js
+++ models/Class.js
@@ -3,7 +3,10 @@ const Schema = mongoose.Schema;
 
 const ClassSchema = new Schema({
   name: { type: String, required: true, maxLength: 10 },
+  current_sem: { type: String, required: true },
   tutor: { type: Schema.Types.Tutor, required: true, ref: "Tutor" },
+  start_year: { type: String, required: true },
+  end_year: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Class", ClassSchema);
