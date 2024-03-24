686f2b7227223a3c5056cca66353cbf2749b39cd
Rif-7 <muhammadrifan974530@gmail.com>
2024-03-24 15:47:21 +0530

    remove express views setup and files

--- app.js
+++ app.js
@@ -16,10 +16,6 @@ mongoose.connect(mongoDB);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error: "));
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -36,13 +32,8 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  console.log(err);
+  res.json({ error: "An Unexpected Error Occured" });
 });
 
 module.exports = app;
--- models/Class.js
+++ models/Class.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ClassSchema = new Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, maxLength: 10 },
   tutor: { type: Schema.Types.Tutor, required: true, ref: "Tutor" },
 });
 
