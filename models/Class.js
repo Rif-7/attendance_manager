c16104e93c0f4e67571eec51a5a04b9509b84cf4
Rif-7 <muhammadrifan974530@gmail.com>
2024-03-25 21:53:20 +0530

    update TutorSchema to get class field as a virtual field

--- models/Class.js
+++ models/Class.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const ClassSchema = new Schema({
   name: { type: String, required: true, maxLength: 10 },
   current_sem: { type: String, required: true },
-  tutor: { type: Schema.Types.Tutor, required: true, ref: "Tutor" },
+  tutor: { type: Schema.Types.ObjectId, required: true, ref: "Tutor" },
   start_year: { type: String, required: true },
   end_year: { type: String, required: true },
 });
--- models/Tutor.js
+++ models/Tutor.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
+const Class = require("./Class");
 const Schema = mongoose.Schema;
 
 const TutorSchema = new Schema({
   f_name: { type: String, required: true, maxLength: 25 },
   l_name: { type: String, require: true, maxLength: 25 },
+});
+
+TutorSchema.virtual("class", {
+  ref: "Class",
+  localField: "_id",
+  foreignField: "tutor",
+  justOne: true,
 });
 
 module.exports = mongoose.model("Tutor", TutorSchema);
