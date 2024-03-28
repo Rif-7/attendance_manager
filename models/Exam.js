934a32cefe18ce3eded288a7237dcaaf833f7b59
Rif-7 <muhammadrifan974530@gmail.com>
2024-03-28 21:31:09 +0530

    add ExamSchema

---
+++ models/Exam.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
+
+const ExamSchema = new Schema({
+  name: { type: String, required: true, maxLength: 25 },
+  date: { type: Date, required: true },
+  time: { type: String, enum: ["FN", "AN"], required: true },
+  class: { type: Schema.Types.ObjectId, ref: "Class", required: true },
+});
+
+ExamSchema.virtual("date_formatted").get(function () {
+  return DateTime.fromJSDate(this.date).setLocale("en-gb").toLocaleString();
+});
+
+module.exports = mongoose.model("Exam", ExamSchema);
