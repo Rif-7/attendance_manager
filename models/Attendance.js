995ef05dcb86c352f5d64f5f1ab3657b9d29bfad
Rif-7 <muhammadrifan974530@gmail.com>
2024-03-29 21:47:05 +0530

    add AttendanceSchema to manage student attendance

---
+++ models/Attendance.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
+
+const AttendanceSchema = new Schema({
+  exam: { type: Schema.Types.ObjectId, required: true, ref: "Exam" },
+  student: { type: Schema.Types.ObjectId, required: true, ref: "Student" },
+  time: { type: Date, default: Date.now, required: true },
+});
+
+AttendanceSchema.virtual("time_formatted").get(function () {
+  return DateTime.fromJSDate(this.time).toLocaleString(DateTime.DATETIME_MED);
+});
+
+module.exports = mongoose.model("Attendance", AttendanceSchema);
